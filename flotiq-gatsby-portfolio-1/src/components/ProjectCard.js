9c444b4741e7e4a1e01a3a1561433737fe067359
mushroom-bois <153285231+mushroom-bois@users.noreply.github.com>
2023-12-08 09:48:00 -0500

    Add files via upload

---
+++ flotiq-gatsby-portfolio-1/src/components/ProjectCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { Card } from 'flotiq-components-react';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+
+const ProjectCard = ({ onClick, name, excerpt, image, slug }) => {
+    const imageSize = { width: 296, height: 296 };
+    return (
+        <Link to={`/${slug}`} className="basis-full md:basis-2/4 lg:basis-1/4">
+            <Card
+                onClick={onClick}
+                bordered={false}
+                additionalClasses={['px-1 cursor-pointer bg-white mb-5']}
+            >
+                {image
+                    && (
+                        <GatsbyImage
+                            image={getImage(image)}
+                            alt={name}
+                            imgClassName={['order-1 lg:order-2']}
+                            imageadditionalprops={imageSize}
+                        />
+                    )}
+                <Card.Body additionalClasses={['px-0 md:px-0']}>
+                    <div>
+                        <Card.Title additionalClasses={['uppercase my-3 text-xl md:text-2xl']}>
+                            {name}
+                        </Card.Title>
+                        <Card.Text
+                            additionalClasses={['text-sm md:text-base font-sora font-light line-clamp-1']}
+                        >
+                            <span dangerouslySetInnerHTML={{ __html: excerpt }} />
+                        </Card.Text>
+                    </div>
+                </Card.Body>
+            </Card>
+        </Link>
+    );
+};
+
+export default ProjectCard;
---
+++ flotiq-gatsby-portfolio-1/src/templates/portfolio-project.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { Header, Paragraph } from 'flotiq-components-react';
+import { Helmet } from 'react-helmet';
+import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image';
+import Layout from '../layouts/layout';
+import ProjectBackButton from '../components/project/ProjectBackButton';
+import Contact from '../components/Contact';
+import ProjectGallery from '../components/project/ProjectGallery';
+
+const PortfolioProjectTemplate = ({ data }) => {
+    const { project } = data;
+    return (
+        <Layout additionalClass={['bg-white']}>
+            <Helmet>
+                <title>{project.name}</title>
+                <meta
+                    name="description"
+                    content={project.description}
+                />
+            </Helmet>
+            <div className="flex flex-wrap max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <ProjectBackButton additionalClass={['my-5']} backButtonText="Back to the main page" />
+            </div>
+            <div className="flex flex-wrap max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 pb-10">
+                <div className="basis-full md:basis-1/2">
+                    {project.headerImage[0]
+                        && (
+                            <GatsbyImage
+                                image={getImage(project.headerImage[0] && project.headerImage[0].localFile)}
+                                stretched="true"
+                                alt={project.name}
+                            />
+                        )}
+                </div>
+
+                <div className="flex flex-col basis-full md:basis-1/2 pl-0 md:pl-12 py-5">
+                    <Header
+                        additionalClasses={['uppercase mb-12 !text-3xl md:!text-4xl lg:!text-5xl']}
+                    >
+                        {project.name}
+                    </Header>
+                    <Paragraph
+                        additionalClasses={['!text-base md:!text-lg font-light font-sora']}
+                    >
+                        {project.description}
+                    </Paragraph>
+                </div>
+            </div>
+            <ProjectGallery
+                galleryName={project.gallery_name}
+                galleryDescription={project.gallery_description}
+                additionalClass={['text-base md:']}
+                gallery={project.gallery}
+                name={project.name}
+            />
+            <div className="flex flex-wrap md:flex-nowrap justify-center max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-20">
+                <Contact
+                    heading="Let's work together"
+                    namInputLabel="Name"
+                    messageInputLabel="Message"
+                    buttonLabel="Send"
+                    imageAlt={project.name}
+                    additionalClass={['']}
+                />
+                <StaticImage
+                    src="../assets/contact-image.jpg"
+                    width={499}
+                    height={288}
+                    alt="contact"
+                    className={['w-auto ml-5 max-h-72 hidden md:block']}
+                    placeholder="none"
+                />
+            </div>
+        </Layout>
+    );
+};
+
+export const pageQuery = graphql`
+    query PortfolioProjectBySlug($slug: String!) {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+        project( slug: { eq: $slug } ) {
+            id
+            description
+            slug
+            name
+            headerImage {
+                extension
+                url
+                width
+                height
+                localFile {
+                    publicURL
+                    childImageSharp {
+                        gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
+                    }
+                }
+            }
+            gallery_name
+            gallery_description
+            gallery {
+                extension
+                url
+                width
+                height
+                id
+                localFile {
+                  publicURL
+                  childImageSharp {
+                  gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
+                }
+                }
+            }
+        }
+    }
+`;
+
+export default PortfolioProjectTemplate;
